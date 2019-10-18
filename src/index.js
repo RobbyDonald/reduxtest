e5ea76e0f0a54c982c52c1d370149e01cd22696a
robbydonald <littlepig1012@outlook.com>
2019-10-18 09:55:41 -0400

    my first redux project

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import './index.css';
+//import { composeWithDevTools } from 'redux-devtools-extension';
+//import App from './App';
+//import MyRoute from './example';
+import * as serviceWorker from './serviceWorker';
+import App from './App.js';
+
+const initialState = {
+  count:0
+};
+
+const reducer =(state=initialState, action)=>{
+ 
+  switch(action.type){
+     case 'INCREMENT':
+       return Object.assign({}, state, {count: state.count+1});
+     default:
+       return state;
+  }  
+}
+
+const store = createStore(reducer,  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+export default store;
+
+ReactDOM.render(<App  />, document.getElementById('root'));
+
+//
+serviceWorker.unregister();
+
+
+
